68ea1e678c2106193d9a203932b2cd8f5b119081
marklihere <mark.li@usu.edu>
2016-10-14 17:26:26 -0600

    Issues with POLLLOOP

--- main.s
+++ main.s
@@ -1,29 +1,3 @@
-; main.s
-; Runs on any Cortex M processor
-; A very simple first project implementing a random number generator
-; Daniel Valvano
-; May 4, 2012
-
-;  This example accompanies the book
-;  "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
-;  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2012
-;  Section 3.3.10, Program 3.12
-;
-;Copyright 2012 by Jonathan W. Valvano, valvano@mail.utexas.edu
-;   You may use, edit, run or distribute this file
-;   as long as the above copyright notice remains
-;THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
-;OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
-;MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
-;VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
-;OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-;For more information about my classes, my research, and my books, see
-;http://users.ece.utexas.edu/~valvano/
-
-
-
-
-
        THUMB
        AREA    DATA, ALIGN=2
        ALIGN          
@@ -41,7 +15,6 @@ MESSAGE DCB "Nope",0000000000000000000000000000000000000000
 	DCB "Stop bothering me",000000000000000000000000000
 	DCB "Not if you were the last person on earth",0000
 	ALIGN
-
 
 RCGC2 equ 0x400FE108
 RCGCUART EQU 0x400FE618
@@ -63,69 +36,43 @@ UART1FBRD	EQU 0x4000D028
 UART1LCRH	EQU 0x4000D02C
 UART1CTL	EQU 0x4000D030
 
-	   
+
+; Start coding
 Start  
-	mov32 R0, #0x400FE108 ; Enable GPIO Clock
-	mov R1, #0x22         ; Enable GPIO B (for UART) and GPIO F (for SW2)
-	str R1, [R0]
+	BL CHIPINIT
+	BL POLLBOTH
+
+	LSL R0, #1
+	LDR R1, =0x40025008  ; RED LED
+LEDON
+	STR R0, [R1]         ; LED ON
+	B LEDON
 	
-	; GPIOF Init
-	mov32 R0, #0x40025000
-	mov32 R1, #0x4C4F434B
-	str R1, [R0,#0x520];GPIO unlock
-	mov R1, #0x1F
-	str R1, [R0,#0x524];GPIOCR
-	mov R1, #0x11
-	str R1, [R0,#0x510]
-	mov R1, #0xFE       ; PF0 [SW2] is input
-	str R1, [R0,#0x400] ;GPIODIR
-	mov R1, #0x1F
-	str R1, [R0,#0x51C] ;digital enable
 
-	; UART Init
-	LDR R0, =RCGCUART
-	mov R1, #0x02
-	str R1, [R0]       ; Activate UART0
-	LDR R0, =UART1CTL
-	MOV32 R1, 0xFFFFFFFE
-	LDR R2, [R0] ; 
-	AND R1, R2, R1       ;
-	STR R1, [R0]  ;   Disable UART1
-			
-	; CFG UART	
-	LDR R0, =UART1IBRD
-	MOV R1, #0x68   ; IIBRD=int(16000000 / (16*9600)) = int(104.166666667) = 0x68
-	STR R1, [R0]
-	LDR R0, =UART1FBRD
-	MOV R1, #0xB   ; int(0.166666667 * 64 + 0.5) = int(11.166666688) = 11  = 0xB
-	STR R1, [R0]
-	LDR R0, =UART1LCRH
-	MOV R1, #0x70
-	STRB R1, [R0, #0x0] ; enable FIFO buffers, 8-bits
-	LDR R0, =UART1CTL
-	MOV32 R1, 0x00000001
-	LDR R2, [R0] ; 
-	ORR R1, R2, R1       ;
-	STR R1, [R0]  ;   ENable UART1
-
-	; en alt func for PB[1:0]
-	LDR R0, =GPIOBAMSEL
-	LDR R1, [R0]
-	AND R1, #3
-	STRB R1, [R0, #0]     ; Enable alternative function for PB[1:0]
-	LDR R0, =GPIOBAFSEL
-	LDR R1, [R0]
-	AND R1, #3
-	STRB R1, [R0, #0]     ; Enable alternative function for PB[1:0]
-	LDR R0, =GPIOBDEN
-	MOV R1, #3
-	STR R1, [R0]          ; Digital enable PB[1:0]
-
-; Start coding sadly at 4pm
-
-; start sighing heavily at 4:15pm
-
-; Poll button & serial input section
+;------------------------------------------
+; Function to Poll button & serial input section
+; INPUT: NOTHING   OUTPUT:  R0 = 1 if ready to send response
+POLLBOTH
+	PUSH {R4, R5, R6, R7, R8, R9, R10, LR}
+	; Poll button
+POLLLOOP
+	LDR R1, =0x40025004    ; access PF[0] (SW2)
+	LDR R2, [R1]
+	CMP R2, #0x0           ; PF[0] == 0 if pressed
+	BEQ READYTORESPOND
+	; Poll Uart Rx Data
+	; Check RXFE == 1, if empty, keep looping
+	; if RXFE == 0 , then already received data from computer
+	; bit [4] of 0x4000D018
+	; 0x42000000 + 32*D018 + 4*4 = 0x421A0310
+	LDR R1, =0x421A0310
+	LDR R2, [R1]
+	CMP R2, #0x1          ; 
+	BEQ POLLLOOP
+READYTORESPOND
+	MOV R0, #1
+	POP {R4, R5, R6, R7, R8, R9, R10, LR}	
+	BX LR
 
 ; Modulo function
 udiv10
@@ -147,10 +94,113 @@ udiv10
 	BX LR
 
 ; print message to serial port
+; uses modulo10 to randomly select a message
+
 
 ; print carriage return and line feed function
+PRINTCR
+	PUSH {R4, R5, R6, R7, R8, R9, R10, LR}
+	; First check if TXFF == 1 (full)
+TXFULLLOOP
+	; bit [7] of 0x4000D018
+	; 0x42000000 + 32*D018 + 4*7 = 0x421A031C
+	LDR R4, =0x421A031C  ; this is bit-banded TXFF
+	LDR R5, [R4]         ; R5 will be 0x0 or 0x1 due to bit-banding
+	CMP R5, #1
+	BEQ TXFULLLOOP
 
-       B   Start
+	; This means ok to Tx
+	; Tx 0x0D CR
+	LDR R4, =UART1DR
+	MOV R5, #0x0D     ; Carriage return
+	STRB R5, [R4,#0x0]
+	POP {R4, R5, R6, R7, R8, R9, R10, LR}	
+	BX LR
+
+
+	B   Start
+
+	   
+CHIPINIT
+	PUSH {R4, R5, R6, R7, R8, R9, R10, LR}
+
+	mov32 R0, #0x400FE108 ; Enable GPIO Clock
+	mov R1, #0x20         ; GPIO F (for SW2)
+	str R1, [R0]
+	NOP
+	NOP
+	NOP
+
+	; GPIOF Init
+	mov32 R0, #0x40025000  ; GPIOF
+	mov32 R1, #0x4C4F434B
+	str R1, [R0,#0x520]   ;GPIO unlock
+	mov R1, #0x3
+	str R1, [R0,#0x524]  ;GPIOCR  
+	mov R1, #0x1
+	str R1, [R0,#0x510] ; pullup on [0]
+	MOV R1, #0x10
+	STR R1, [R0,#0x514] ; Pulldown LED so it is off initially
+	mov R1, #0xFE       ; PF0 [SW2] is input, PF1 is output
+	str R1, [R0,#0x400] ;GPIODIR
+	mov R1, #0x1F
+	str R1, [R0,#0x51C] ;digital enable
+
+	; UART Init
+	; Enable the UART module using the RCGCUART register
+	LDR R0, =RCGCUART
+	mov R1, #0x02
+	str R1, [R0]       ; Activate UART1
+
+	; Enable the clock to the appropriate GPIO module via the RCGCGPIO register
+	;Enable GPIO B (for UART) and 
+	MOV32 R0, #0x400FE108 ; Enable GPIO Clock
+	mov R1, #0x22         ; Enable GPIOB
+	str R1, [R0]
+
+
+	; en alt func for PB[1:0]
+	LDR R0, =GPIOBAMSEL
+	LDR R1, [R0]
+	MOV32 R2, #0xFFFFFFC    ; EXPLICITLY DO NOT ALLOW ANALOG ON PB[1:0]
+	AND R1, R2
+	STRB R1, [R0, #0]     ; Enable alternative function for PB[1:0]
+	LDR R0, =GPIOBAFSEL
+	LDR R1, [R0]
+	AND R1, #3
+	STRB R1, [R0, #0]     ; Enable alternative function for PB[1:0]
+	; May not be needed?
+	LDR R0, =GPIOBDEN
+	MOV R1, #3
+	STR R1, [R0]          ; Digital enable PB[1:0]
+
+; Disable after enabling alt func and pin for GPIO
+	LDR R0, =0x421A0600    
+	; bitband UART1 to set UARTEN = 0 (turn off)
+	; 0x42000000 + 32*D030 + 7*0 = 0x421A0600
+	MOV R1, #0x0
+	STR R1, [R0]  ;   Disable UART1
+
+	; CFG UART
+	MOV32 R2, #0x4000D024
+	;LDR R2, =UART1IBRD
+	MOV32 R1, #0x68   ; IIBRD=int(16000000 / (16*9600)) = int(104.166666667) = 0x68
+	STR R1, [R2]
+	LDR R0, =UART1FBRD
+	MOV R1, #0xB   ; int(0.166666667 * 64 + 0.5) = int(11.166666688) = 11  = 0xB
+	STR R1, [R0]
+	LDR R0, =UART1LCRH
+	MOV32 R1, #0x70
+	STRB R1, [R0, #0x0] ; enable FIFO buffers, 8-bits
+	LDR R0, =UART1CTL
+	MOV32 R1, 0x00000001
+	LDR R2, [R0] ; 
+	ORR R1, R2, R1       ;
+	STR R1, [R0]  ;   ENable UART1
+
+	POP {R4, R5, R6, R7, R8, R9, R10, LR}	
+	BX LR
+	
 
        ALIGN      
        END  
